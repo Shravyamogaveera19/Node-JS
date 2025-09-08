15544e871d87d89d0270ad135d09d33e59252369
Shravya S Mogaveera <shravyasmogaveera@gmail.com>
2025-09-08 22:43:58 +0530

    Using Mongodb database

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,17 +1,21 @@
 const Tour = require('../models/tourModel');
 
-exports.getAlltours = (req, res) => {
-  console.log(req.params);
-  console.log(req.requestTime);
-
+exports.getAlltours = async (req, res) => {
+try{
+  const tours = await Tour.find();
   res.status(200).json({
     status: 'success',
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
+    results: tours.length,
+    data: {
+      tours
+    }
   });
+}catch(err){
+  res.status(404).json({
+    status:'fail',
+    message: err
+  });
+}
 };
 
 exports.createTour = async(req, res) => {
@@ -31,17 +35,23 @@ exports.createTour = async(req, res) => {
   }
 };
 
-exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  // const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
+exports.getTour = async(req, res) => {
+  try{
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
     status: 'success',
-    // data: {
-    //   tour,
-    // },
+    data:{
+      tour
+    }
   });
+  } catch(err){
+    res.status(404).json({
+      status:'fail',
+      message:err
+    });
+  }
 };
+
 exports.updateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
