d741308eb50eaecbb26307e04f9058a19d1a5a2a
Shravya S Mogaveera <shravyasmogaveera@gmail.com>
2025-09-11 23:58:32 +0530

    Updated tourController

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -73,19 +73,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = async(req, res) => {
-  try{
-  await Tour.findByIdAndDelete(req.params.id)
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-}
-catch(err){
-  res.status(400).json({
-    status:'fail',
-    message:'Invalid data sent'
-  })
-}
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
 };
