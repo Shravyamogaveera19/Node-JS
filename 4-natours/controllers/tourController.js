cceda9994b37e5e30381d5adbb96fa3824eaa5bd
Shravya S Mogaveera <shravyasmogaveera@gmail.com>
2025-09-14 23:48:38 +0530

    Updated Tour Controller

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -1,8 +1,36 @@
 const Tour = require('../models/tourModel');
 
 exports.getAlltours = async (req, res) => {
+
   try {
-    const tours = await Tour.find();
+
+    console.log(req.query);
+    //BUILD QUERY
+    //1)Filtering
+    const queryObj = {...req.query};
+    const excludedFields = ['page','sort','limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    //2)Advances Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match =>`$${match}}`);
+    console.log(JSON.parse(queryStr));
+
+    //{difficulty: 'easy', duration: {$gte: 5}}
+    //{difficulty: 'easy', duration: {gte:'5'}}
+    //gte,gt,lte,lt
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    //EXECUTE QUERY
+    const tours = await query;
+    // const query = Tour.find()
+    // .where('duration')
+    // .equals(5)
+    // .where('difficulty')
+    // .equals('easy')
+
+    //SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
