573090db1e256b16f47279702518109a9220c373
Shravya S Mogaveera <shravyasmogaveera@gmail.com>
2025-09-15 23:51:47 +0530

    Updated Tour controller

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -24,11 +24,6 @@ exports.getAlltours = async (req, res) => {
 
     //EXECUTE QUERY
     const tours = await query;
-    // const query = Tour.find()
-    // .where('duration')
-    // .equals(5)
-    // .where('difficulty')
-    // .equals('easy')
 
     //SEND RESPONSE
     res.status(200).json({
