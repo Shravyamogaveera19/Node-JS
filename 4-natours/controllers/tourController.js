ed093c988037d17221acb7c07c1493c62c726ff7
Shravya S Mogaveera <shravyasmogaveera@gmail.com>
2025-09-26 23:54:31 +0530

    Updated tourController

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -20,7 +20,6 @@ exports.getAlltours = async (req, res) => {
     else{
       query = query.sort('-createdAt');
     }
-
     //3)Field limiting 
     if(req.query.fields){
       const fields = req.query.fields.split(',').join(' ');
